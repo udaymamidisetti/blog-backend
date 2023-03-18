f78bdebbb45aca672697f743d3840c8adb48c51c
udaymamidisetti <udaymamidisetti5@gmail.com>
2023-03-18 18:09:36 +0530

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { urlencoded } = require("express");
 const authController = require("./controllers/authController");
 const blogController = require("./controllers/blogController");
+const multer = require("multer");
 const dotenv = require("dotenv").config();
 const app = express();
 app.use(cors());
@@ -12,4 +13,19 @@ app.use(express.json());
 mongoose.connect(process.env.MONGOOSE_URL).then(() => console.log("Connected"));
 app.use("/auth", authController);
 app.use("/blog", blogController);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.body.filename);
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+app.post("/upload", upload.single("image"), async (req, res) => {
+  return res.status(200).json({ msg: "Successsfully Uploaded" });
+});
 app.listen(process.env.PORT, () => console.log("Server Started"));
